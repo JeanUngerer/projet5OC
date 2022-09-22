671eb4c8617b6fb47dbb067d4d1e84228f7fc8a4
Jean UNGERER <j.ungerer@technologyandstrategy.com>
2022-09-22 13:39:34 +0200

    configure and try logger

--- src/main/java/com/SafetyNet/Alerts/persons/controller/PersonsController.java
+++ src/main/java/com/SafetyNet/Alerts/persons/controller/PersonsController.java
@@ -4,5 +4,5 @@ import org.springframework.http.ResponseEntity;
 
 
 public interface PersonsController {
-
+	void getUnitsById();
 }
--- src/main/java/com/SafetyNet/Alerts/persons/controller/PersonsControllerImpl.java
+++ src/main/java/com/SafetyNet/Alerts/persons/controller/PersonsControllerImpl.java
@@ -1,5 +1,8 @@
 package com.SafetyNet.Alerts.persons.controller;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,5 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("${spring.data.rest.base-path}")
 public class PersonsControllerImpl {
+	
+	
+	private static final Logger LOGGER =  LogManager.getLogger(PersonsControllerImpl.class);
+	
+	public static void main() {
+		LOGGER.info("APPEL");
+	}
+	
+	@GetMapping("/units")
+	public void getUnitsById() {
+		LOGGER.info("APPEL");
+		
+	}
 
 }
